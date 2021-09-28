61b0c96518a99596c1eb32113f957960ebe9d513
Raymw1 <rayan.rmw@gmail.com>
2021-09-28 15:51:56 -0300

    Stats memory

---
+++ stats.js
@@ -0,0 +1,22 @@
+const os = require("os");
+const { freemem, totalmem } = os;
+
+setInterval(() => {
+  const freeMemory = freemem() / 1024 ** 2;
+  const totalMemory = totalmem() / 1024 ** 2;
+  const memoryUsage = (freeMemory / totalMemory) * 100;
+  // console.log(`Free memory: ${freeMemory} MB`);
+  // console.log(`Total memory: ${totalMemory} MB`);
+  // console.log(`Memory usage: ${memoryUsage}%`);
+  // console.log(os.platform());
+
+  const stats = {
+    free: `${freeMemory} MB`,
+    total: `${totalMemory} MB`,
+    usage: `${memoryUsage}%`,
+  };
+
+  console.clear();
+  console.log("\n======= PC STATS =======");
+  console.table(stats);
+}, 1000);
